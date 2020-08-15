d41c2c47a01374edf153de942bda39c579890bf6
DJWOMS <socanime@gmail.com>
2020-08-15 22:10:35 +0300

    updated ip

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        backendUrl: "http://127.0.0.1:1313/api/v1"
+        backendUrl: "http://164.90.190.249/api/v1"
     },
     mutations: {},
     actions: {},
